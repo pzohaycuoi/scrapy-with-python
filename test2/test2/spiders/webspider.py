b610aff552ab4a5a538a8d78479226b49f9cbee1
pzohaycuoi <pzohaycuoi@gmail.com>
2020-12-04 21:36:52 +0700

    2nd days

--- test2/test2/spiders/webspider.py
+++ test2/test2/spiders/webspider.py
@@ -1,13 +1,17 @@
 import scrapy
 
-class webscrapper (scrapy.Spider):
-    name = "webURL"
+class yoSpidy(scrapy.Spider):
+    name = "scrapWebsite"
     start_urls = [
-        'https://alonhadat.com.vn/nha-dat/can-ban/nha-dat/1/ha-noi.html'
+        'https://alonhadat.com.vn/nha-dat/can-ban/nha-dat/1/ha-noi.html',
     ]
 
     def parse(self, response):
-        for items in response.xpath('//*[@class="content-item"]'):
+        for items in response.css("div.content-item"): 
             yield {
-                'title': items.xpath('//*[@class="ct_title"]/text()').get()
+                'title': items.css("div.ct_title a::text").get(),
+                'road-width': items.css("span.road-width::text").get(),
+                'floors': items.css("span.floors::text").get(),
+                'bedroom': items.css("span.bedroom::text").get(),
+                'ct_dt': items.css("div.ct_dt label::text").get(),
             }
